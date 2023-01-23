6aa960d85d828e6da210a332f833c2f26f7f8aea
Adentalis <daniel.n96@t-online.de>
2023-01-23 23:22:53 +0100

    show planet sizes

--- plugin/features/planetSizes.js
+++ plugin/features/planetSizes.js
@@ -4,9 +4,16 @@ if (planetList) {
   const planets = planetList.getElementsByTagName('div');
 
   for (const planet of planets) {
-    console.log(planet);
-    let a = document.createElement('span');
-    a.textContent = 'eu';
-    planet.insertAdjacentElement('afterend', a);
+    let planetLink = planet.getElementsByClassName('planetlink');
+
+    if (planetLink.length > 0) {
+      planetTitle = planetLink[0].getAttribute('title');
+      let a = document.createElement('span');
+      a.textContent = planetTitle.slice(
+        planetTitle.indexOf('(') + 1,
+        planetTitle.indexOf(')')
+      );
+      planet.insertAdjacentElement('afterend', a);
+    }
   }
 }
