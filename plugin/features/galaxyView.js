c239df87431ef5369125a8ff79d764b56e940ed4
Adentalis <daniel.n96@t-online.de>
2023-01-24 23:15:59 +0100

    show player rank in galaxy

---
+++ plugin/features/galaxyView.js
@@ -0,0 +1,46 @@
+const galaxy = document.querySelector('#galaxyContent');
+
+if (galaxy) {
+  let galaxyRows = galaxy.getElementsByClassName('ctContentRow');
+  for (row of galaxyRows) {
+    if (!row.classList.contains('empty_filter')) {
+      let rank = getRank(row);
+      let targetSpan = getTargetSpan(row);
+      let a = document.createElement('span');
+      a.textContent = '  ' + rank;
+      targetSpan.insertAdjacentElement('beforeend', a);
+    }
+  }
+}
+
+function getRank(row) {
+  let player = row.getElementsByClassName('cellPlayerName');
+  if (player.length === 1) {
+    let rank = player[0].getElementsByClassName('rank');
+    if (rank.length === 1) {
+      return rank[0].innerText.replace(/\D/g, '');
+    } else {
+      return '';
+    }
+  }
+}
+
+function getTargetSpan(row) {
+  let player = row.getElementsByClassName('cellPlayerName');
+  if (player.length === 1) {
+    let allSpans = player[0].getElementsByTagName('span');
+    return allSpans[0];
+  }
+}
+
+/**
+ *
+ * Bugs:
+ * - my own rank is buggy
+ * - update after system is changed
+ *
+ *
+ * Todo:
+ *  - refactor
+ *  - show ally information
+ */
