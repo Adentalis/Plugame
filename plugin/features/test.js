61f3005607f116fcc9548704d4c0eaa5651af0fc
Adentalis <daniel.n96@t-online.de>
2023-01-26 18:05:13 +0100

    click on test button redirects to landing pagee

--- plugin/features/test.js
+++ plugin/features/test.js
@@ -5,9 +5,17 @@ if (sidebar) {
   testbutton.textContent = 'Plugin Working';
   testbutton.setAttribute('class', 'textlabel');
   testbutton.style.color = 'red';
+  testbutton.onclick = on;
 
   const menu = document.createElement('div');
   menu.appendChild(testbutton);
 
   sidebar.insertAdjacentElement('beforebegin', menu);
+}
+
+function on() {
+  const menuItems = sidebar.getElementsByTagName('li');
+  const l = menuItems[0].getElementsByClassName('menubutton');
+  const toClick = l[0];
+  toClick.click();
 }
