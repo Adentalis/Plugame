3d04ea02b75f7738e13414b058ec833b6c37c18c
Adentalis <daniel.n96@t-online.de>
2023-01-30 22:03:59 +0100

    live update expo arrival time

--- plugin/features/planetSizes.js
+++ plugin/features/planetSizes.js
@@ -1,47 +1,30 @@
-const planetList = document.querySelector('#planetList');
-let eventResult = [];
-const eventContent2 = document.getElementsByClassName('eventFleet');
-if (eventContent2) {
-  for (row of eventContent2) {
-    let originFleet = row.getElementsByClassName('originFleet');
-    let startPlanet = originFleet[0].innerText.trim();
+let firstIteration = true;
+let planetIndex = 0;
 
-    let imgColumn = row.getElementsByClassName('missionFleet');
-    let img = imgColumn[0].getElementsByTagName('img');
-    let isExpedition = img[0].getAttribute('title').includes('Expedition');
-    let isReturningFleet = row.getAttribute('data-return-flight') === 'true';
+function test() {
+  planetIndex = 0;
+  const planetList = document.querySelector('#planetList');
 
-    if (isExpedition && isReturningFleet) {
-      let t = row.getAttribute('data-arrival-time');
-      let date = new Date(parseInt(t * 1000));
-      let now = new Date();
-      let diff = date - now;
-      eventResult.push({
-        name: startPlanet,
-        arrival: millisToMinutesAndSeconds(diff),
-      });
-    }
-  }
-}
+  let eventResult = checkForEachPlanetExpoArrivalTime();
 
-if (planetList) {
   const planets = planetList.getElementsByTagName('div');
 
   for (const planet of planets) {
+    planetIndex++;
     let planetLink = planet.getElementsByClassName('planetlink');
 
-    if (planetLink.length > 0) {
-      planetTitle = planetLink[0].getAttribute('title');
-      let resultString = planetTitle.slice(
-        planetTitle.indexOf('(') + 1,
-        planetTitle.indexOf(')')
-      );
+    planetTitle = planetLink[0].getAttribute('title');
+    let resultString = planetTitle.slice(
+      planetTitle.indexOf('(') + 1,
+      planetTitle.indexOf(')')
+    );
 
-      const pName = planet.getElementsByClassName('planet-name')[0].innerHTML;
-      const expo = eventResult.find((e) => e.name === pName);
-      const toWrite = expo ? `Returns in ${expo.arrival}` : 'no expo';
-      const color = expo ? 'green' : 'red';
+    const pName = planet.getElementsByClassName('planet-name')[0].innerHTML;
+    const expo = eventResult.find((e) => e.name === pName);
+    const toWrite = expo ? `Returns in ${expo.arrival}` : 'no expo';
+    const color = expo ? 'green' : 'red';
 
+    if (firstIteration) {
       const fieldSpan = document.createElement('span');
       fieldSpan.textContent = resultString;
       fieldSpan.style.position = 'absolute';
@@ -49,6 +32,7 @@ if (planetList) {
       fieldSpan.style.fontSize = '0.85em';
 
       const expoSpan = document.createElement('span');
+      expoSpan.setAttribute('id', 'planetFields' + planetIndex);
       expoSpan.textContent = toWrite;
       expoSpan.style.position = 'absolute';
       expoSpan.style.marginLeft = '50px';
@@ -58,10 +42,21 @@ if (planetList) {
 
       planet.insertAdjacentElement('afterend', fieldSpan);
       planet.insertAdjacentElement('afterend', expoSpan);
+    } else {
+      spanToChange = document.querySelector('#planetFields' + planetIndex);
+      spanToChange.textContent = toWrite;
+      spanToChange.style.color = color;
     }
   }
+  firstIteration = false;
 }
 
+function inter() {
+  setInterval(() => {
+    test();
+  }, 1000);
+}
+inter();
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000);
   var seconds = ((millis % 60000) / 1000).toFixed(0);
@@ -78,6 +73,34 @@ async function sayHi() {
   chrome.storage.local.get('t').then((result) => {
     console.log('Value currently is ' + JSON.stringify(result));
   });
+}
+
+function checkForEachPlanetExpoArrivalTime() {
+  const eventContent2 = document.getElementsByClassName('eventFleet');
+  let res = [];
+  if (eventContent2) {
+    for (row of eventContent2) {
+      let originFleet = row.getElementsByClassName('originFleet');
+      let startPlanet = originFleet[0].innerText.trim();
+
+      let imgColumn = row.getElementsByClassName('missionFleet');
+      let img = imgColumn[0].getElementsByTagName('img');
+      let isExpedition = img[0].getAttribute('title').includes('Expedition');
+      let isReturningFleet = row.getAttribute('data-return-flight') === 'true';
+
+      if (isExpedition && isReturningFleet) {
+        let t = row.getAttribute('data-arrival-time');
+        let date = new Date(parseInt(t * 1000));
+        let now = new Date();
+        let diff = date - now;
+        res.push({
+          name: startPlanet,
+          arrival: millisToMinutesAndSeconds(diff),
+        });
+      }
+    }
+  }
+  return res;
 }
 
 sayHi();
