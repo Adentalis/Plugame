9d01b355504a1c2faac72bd58fb59f976e034e40
Adentalis <daniel.n96@t-online.de>
2023-03-12 03:26:22 +0100

    remove old stuff + refactor

--- plugin/features/mines.js
+++
@@ -1,7 +0,0 @@
-const minesView = document.querySelector('#technologies');
-
-if (minesView) {
-  let a = document.createElement('span');
-  a.textContent = 'mines';
-  minesView.insertAdjacentElement('beforebegin', a);
-}
--- plugin/features/pluginLoaded.js
+++ plugin/features/pluginLoaded.js
@@ -2,20 +2,18 @@ const sidebar = document.querySelector('#menuTable');
 
 if (sidebar) {
   const testbutton = document.createElement('button');
-  testbutton.textContent = 'Plugin Working';
+  testbutton.textContent = 'Plugin Loaded!';
   testbutton.setAttribute('class', 'textlabel');
   testbutton.style.color = 'red';
-  testbutton.onclick = on;
+  testbutton.onclick = goDashboard;
 
   const menu = document.createElement('div');
   menu.appendChild(testbutton);
-
   sidebar.insertAdjacentElement('beforebegin', menu);
 }
 
-function on() {
+function goDashboard() {
   const menuItems = sidebar.getElementsByTagName('li');
-  const l = menuItems[0].getElementsByClassName('menubutton');
-  const toClick = l[0];
-  toClick.click();
+  const menubutton = menuItems[0].getElementsByClassName('menubutton');
+  menubutton[0].click();
 }
